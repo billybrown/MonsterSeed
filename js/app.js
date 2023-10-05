6952da0c73425172164c9b2114a41009229969bb
Bill Brown <billybrown00@gmail.com>
2023-10-05 11:05:31 -0500

    eating flowers

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 
 
 var flowers = document.getElementsByClassName("flower");
+var monster = document.getElementById("monster");
 
 var harvest = function() {
-    this.classList.add('harvested');
+    var flower = this;
+    flower.classList.add('js-harvested');
+    setTimeout(function(){
+        flower.classList.add('js-gone');
+    }, 750);
+
+    monster.classList.add('js-feed');
+    setTimeout(function(){
+        monster.classList.remove('js-feed');
+    }, 1300);
 };
 
 for (var i = 0; i < flowers.length; i++) {
